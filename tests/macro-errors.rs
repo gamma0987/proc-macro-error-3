094f4c1e7dc12405b4c58769da1b3899cc1643a8
gamma0987 <gamma0987@posteo.de>
2026-06-08 20:00:45 +0200

    tests: Add tests for all trailing commas cases

--- tests/macro-errors.rs
+++ tests/macro-errors.rs
@@ -3,4 +3,5 @@
 fn ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
+    t.pass("tests/ui-pass/*.rs");
 }
---
+++ tests/ui/abort_trailing.rs
@@ -0,0 +1,8 @@
+use test_crate::*;
+
+abort_trailing!(one);
+abort_trailing_fmt!(one);
+abort_trailing_notes!(one, two);
+abort_trailing_from!(one);
+
+fn main() {}
